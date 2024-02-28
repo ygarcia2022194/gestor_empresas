2a723eb8587d681fa37ba9c100672b8af08bdd09
ygarcia-2022194 <ygarcia-2022194@kinal.edu.gt>
2024-02-28 16:06:33 -0600

    Se hicieron las validaciones para las rutas de user

---
+++ src/helpers/db-validators.js
@@ -0,0 +1,15 @@
+import User from "../user/user";
+
+export const existeEmail = async (correo='')=>{
+    const existeEmail = await User.findOne({correo});
+    if(existeEmail){
+        throw new Error(`El email ${correo} ya fue registrado`);
+    }
+}
+
+export const existeUsuarioById = async (id = '')=>{
+    const existeUsuario = await User.findById(id);
+    if(!existeUsuario){
+        throw new Error(`El id: ${correo}} ya fue registrado`);
+    }
+}
