b18bc751fb532f7dfe272789013be2f00ad2de72
ygarcia-2022194 <ygarcia-2022194@kinal.edu.gt>
2024-02-28 15:58:42 -0600

    Se hizo el helper que valida los campos

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -6,7 +6,7 @@ export const userPost = async (req, res)=>{
     const {nombre, correo, password} = req.body;
     const user = new User({nombre, correo, password});
     const salt = bcryptjs.genSaltSync();
-    user.password = bcryptjs.hashSync(password, salt);\
+    user.password = bcryptjs.hashSync(password, salt);
     await user.save();
 
     res.status(200).json({
